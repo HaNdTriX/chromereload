053655946cd3161609102c6f53a88c9b830c0937
HaNdTriX <handtrix@gmail.com>
2016-09-17 09:34:17 +0200

    Add Source Maps

--- devonly.js
+++ devonly.js
@@ -9,3 +9,4 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 if (process.env.NODE_ENV === 'development') {
   new _chromereload2.default();
 }
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInNyYy9kZXZvbmx5LmpzIl0sIm5hbWVzIjpbInByb2Nlc3MiLCJlbnYiLCJOT0RFX0VOViJdLCJtYXBwaW5ncyI6Ijs7QUFBQTs7Ozs7O0FBRUEsSUFBSUEsUUFBUUMsR0FBUixDQUFZQyxRQUFaLEtBQXlCLGFBQTdCLEVBQTRDO0FBQzFDO0FBQ0QiLCJmaWxlIjoiZGV2b25seS5qcyIsInNvdXJjZXNDb250ZW50IjpbImltcG9ydCBDaHJvbWVSZWxvYWQgZnJvbSAnLi9jaHJvbWVyZWxvYWQuanMnO1xuXG5pZiAocHJvY2Vzcy5lbnYuTk9ERV9FTlYgPT09ICdkZXZlbG9wbWVudCcpIHtcbiAgbmV3IENocm9tZVJlbG9hZCgpO1xufVxuIl19
